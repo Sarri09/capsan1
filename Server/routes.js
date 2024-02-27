54ef030ea1e72aef108fb9690f987a5b831fe4c0
Andy <72534502+Sarri09@users.noreply.github.com>
2024-02-27 17:24:14 -0300

    Update routes.js

--- Server/routes.js
+++ Server/routes.js
@@ -1,9 +1,13 @@
 const { request, response } = require('express');
 
 module.exports = function(app) {
+    //registro
     app.post('/', (req, res) => {
         const newusr = req.body;
         console.log(newusr);
+
+        //AGARRA LO QUE ESTA PASANDO POR EL BODY Y MANDALO A EL SERVICIO QUE GESTIONA LA BASE DE DATOS EN UNA
+        //FUNCION QUE INSERTE DATOS EN LA TABLA ()agregarusuario (newusr)
 
         /*.then(() => {
             res.json({"mensaje" : "Usuario registrado con exito"});
